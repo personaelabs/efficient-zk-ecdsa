0cb72c479f6b82f83be6b986fd46f8b40a095a67
Daniel Tehrani <contact@dantehrani.com>
2022-10-09 22:28:37 +0900

    Update benching

--- bench/prover.js
+++ bench/prover.js
@@ -1,22 +1,27 @@
-const wasmFile = `https://storage.googleapis.com/prover_jp/ecdsa_verify.wasm`;
-const zKeyFile = "https://storage.googleapis.com/prover_jp/ecdsa_verify.zkey";
-
-// const wasmFile = "zk-ecdsa.s3.ap-northeast-1.amazonaws.com/ecdsa_verify.wasm";
-// const wasmFile = "zk-ecdsa.s3.ap-northeast-1.amazonaws.com/ecdsa_verify.zkey";
-
-const verify = async () => {
-  console.log("Proving....");
-
+const prove = async (wasmFile, zKeyFile) => {
+  console.log("Proving...");
   console.time("fullProve");
 
-  const fullProof = await snarkjs.groth16
-    .fullProve(sampleInput, wasmFile, zKeyFile)
-    .catch(err => {
-      console.error(err);
-    });
-  console.log(fullProof);
+  const fullProof = await snarkjs.groth16.fullProve(
+    sampleInput,
+    wasmFile,
+    zKeyFile
+  );
 
+  console.log({ fullProof });
   console.timeEnd("fullProve");
+};
 
-  console.log("Done....");
+const verify = async () => {
+  await prove(
+    "https://d2q52de7b4rwg.cloudfront.net/ecdsa_verify.wasm",
+    "https://d2q52de7b4rwg.cloudfront.net/ecdsa_verify.zkey"
+  );
+};
+
+const verifyPubKeyToAddr = async () => {
+  await prove(
+    "https://d2q52de7b4rwg.cloudfront.net/ecdsa_verify_pubkey_to_addr.wasm",
+    "https://d2q52de7b4rwg.cloudfront.net/ecdsa_verify_pubkey_to_addr.zkey"
+  );
 };
